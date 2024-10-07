43d9e76be4144482890e9d58434b962da381e7f1
Rusu Bogdan <rusub2003@gmail.com>
2024-10-07 11:29:56 +0300

    LAB02: do exercise 4

--- laboratoare/lab01/example.c
+++ laboratoare/lab01/example.c
@@ -13,6 +13,18 @@ void *f(void *arg) {
   pthread_exit(NULL);
 }
 
+void *task1(void *arg) {
+  long id = *(long *)arg;
+  printf("Aici avem taskul 1 apelat din threadul %ld\n", id);
+  pthread_exit(NULL);
+}
+
+void *task2(void *arg) {
+  long id = *(long *)arg;
+  printf("Aici avem taskul 2 apelat din threadul %ld\n", id);
+  pthread_exit(NULL);
+}
+
 int main(int argc, char *argv[]) {
   pthread_t threads[NUM_THREADS];
   int r;
@@ -20,24 +32,33 @@ int main(int argc, char *argv[]) {
   void *status;
   long ids[NUM_THREADS];
 
-  for (id = 0; id < NUM_THREADS; id++) {
-    ids[id] = id;
-    r = pthread_create(&threads[id], NULL, f, &ids[id]);
+  // for (id = 0; id < NUM_THREADS; id++) {
+  //   ids[id] = id;
+  //   r = pthread_create(&threads[id], NULL, f, &ids[id]);
 
-    if (r) {
-      printf("Eroare la crearea thread-ului %ld\n", id);
-      exit(-1);
-    }
-  }
+  //   if (r) {
+  //     printf("Eroare la crearea thread-ului %ld\n", id);
+  //     exit(-1);
+  //   }
+  // }
 
-  for (id = 0; id < NUM_THREADS; id++) {
-    r = pthread_join(threads[id], &status);
+  // for (id = 0; id < NUM_THREADS; id++) {
+  //   r = pthread_join(threads[id], &status);
 
-    if (r) {
-      printf("Eroare la asteptarea thread-ului %ld\n", id);
-      exit(-1);
-    }
-  }
+  //   if (r) {
+  //     printf("Eroare la asteptarea thread-ului %ld\n", id);
+  //     exit(-1);
+  //   }
+  // }
+
+  ids[0] = 1;
+  pthread_create(&threads[0], NULL, task1, &ids[0]);
+
+  ids[1] = 2;
+  pthread_create(&threads[1], NULL, task2, &ids[1]);
+
+  pthread_join(threads[0], &status);
+  pthread_join(threads[1], &status);
 
   return 0;
 }
